93ed56f6c557cd7b10e6f21648f98cafadde9e2e
Only1cleve <cleve.king@snhu.edu>
2025-06-04 09:09:37 -0400

    ProcTerrain and dig

--- Source/firstperson_415/firstperson_415Projectile.cpp
+++ Source/firstperson_415/firstperson_415Projectile.cpp
@@ -8,6 +8,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "NiagaraFunctionLibrary.h"
 #include "NiagaraComponent.h"
+#include "PerlinProcTerrain.h"
 
 Afirstperson_415Projectile::Afirstperson_415Projectile()
 {
@@ -74,5 +75,13 @@ void Afirstperson_415Projectile::OnHit(UPrimitiveComponent* HitComp, AActor* Oth
 
 		MatInstance->SetVectorParameterValue("Color", randColor);
 		MatInstance->SetScalarParameterValue("Frame", frameNum);
+
+		APerlinProcTerrain* procTerrain = Cast<APerlinProcTerrain>(OtherActor);
+
+		if (procTerrain)
+		{
+			procTerrain->AlterMesh(Hit.ImpactPoint);
+		}
+
 	}
 }
